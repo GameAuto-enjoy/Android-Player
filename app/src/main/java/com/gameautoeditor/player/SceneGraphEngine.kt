759e07c97ffe86539147157dd5633448a2f68138
Edward <skrapy50115@gmail.com>
2026-01-13 10:51:02 +0800

    chore: bump version to 1.7.4 with apk size optimization and detailed perception logs

--- app/src/main/java/com/gameautoeditor/player/SceneGraphEngine.kt
+++ app/src/main/java/com/gameautoeditor/player/SceneGraphEngine.kt
@@ -41,7 +41,7 @@ class SceneGraphEngine(private val service: AutomationService) {
                     variables[key] = settingsVars.optInt(key, 0)
                 }
             }
-            Log.i(TAG, "🤖 SceneGraphEngine (FSM) 已啟動. 版本: 1.7.3. 變數: $variables")
+            Log.i(TAG, "🤖 SceneGraphEngine (FSM) 已啟動. 版本: 1.7.4. 變數: $variables")
 
             workerThread = Thread { runLoop() }
             workerThread?.start()
